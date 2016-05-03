37b2c578ad95803309867385fa924c9eb2870860
Kostas Minaidis <kostas@creativecollective.gr>
2016-05-04 01:07:49 +0200

    Adding alphabetical index

--- js/app.coffee
+++ js/app.coffee
@@ -3,8 +3,58 @@ request = new XMLHttpRequest
 do ->
 
   $searchInput = document.getElementById 'search'
-  $output = document.getElementById 'results'
-  jargon = {}
+  $output      = document.getElementById 'results'
+  $entriesBox  = document.getElementById 'entries'
+  $$letters    = Array.prototype.slice.apply(document.querySelectorAll('.alphabet ul li a'))
+  $$entries    = []
+  jargon       = {}
+
+  unhighlightLetters = ()->
+
+    $$letters.map (letterLi)-> letterLi.classList.remove 'highlighted' 
+
+  createEntriesSelectableList = (jargon)->
+
+    for key, value of jargon
+      anchor = document.createElement "a"
+      anchor.href = "?term=#{key}"
+      anchor.textContent = key
+      li = document.createElement "li" 
+      li.setAttribute "class", key.slice(0,1).toLowerCase()
+      li.appendChild anchor
+      $entriesBox.appendChild li
+      $$entries = $entriesBox.querySelectorAll 'li'
+      $$entries = Array.prototype.slice.apply $$entries
+
+  createSelectableAlphabet = ()->
+
+    document.getElementById("alphabet")
+    .addEventListener "click", (e)->
+      e.preventDefault()
+      e.stopPropagation()
+      unhighlightLetters() 
+      e.target.classList.add 'highlighted'
+      letter = e.target.textContent.toLowerCase()
+      $output.querySelector('.entry').innerHTML = ""
+      $entriesBox.classList.remove "hide"
+      $entriesBox.classList.add "fold"
+      $$entries.map (li)->
+        if li.classList.contains(letter)
+          li.classList.add 'selected'
+        else 
+          li.classList.remove 'selected'
+
+  displayEntry = (key)->
+
+    $entriesBox.classList.add "hide"
+    $output.querySelector('.entry').innerHTML = jargon[key]
+    location.href = location.href.split("#")[0] + "#" + key
+
+  initializeList = ()->
+    $entriesBox.addEventListener "click", (e)->
+      e.preventDefault()
+      e.stopPropagation()
+      displayEntry(e.target.textContent)
 
   initSearch = (data)->
 
@@ -12,17 +62,23 @@ do ->
       jargon[entry.name] = entry.html
       return entry.name
 
+    # Check URL #hash and load term accordingly
+    if location.hash isnt "" then displayEntry location.hash.slice(1)
+
+    createEntriesSelectableList(jargon)
+    createSelectableAlphabet()
+    initializeList()
+
+    # Initialize filtering
     h = horsey($searchInput, {
         suggestions : entries
         limit       : 8
-        render: (li,suggestion)-> 
-          li.innerHTML = suggestion
         set: (value)->
           $searchInput.value = value
-          $output.innerHTML = jargon[value]
-      })
+          displayEntry(value)
+          unhighlightLetters() 
 
-    $searchInput.addEventListener 'horsey-filter', (e)->
+      })
 
   request.open 'GET', 'js/entries.json', true
 
@@ -36,3 +92,5 @@ do ->
 
   request.send()
 
+# TODO LIST
+# + Trigger term load on URL, e.g. http://kostasx.github.io/SJSJ?term=backbone should automatically load the term Backbone
--- js/app.js
+++ js/app.js
@@ -4,28 +4,88 @@ var request;
 request = new XMLHttpRequest;
 
 (function() {
-  var $output, $searchInput, initSearch, jargon;
+  var $$entries, $$letters, $entriesBox, $output, $searchInput, createEntriesSelectableList, createSelectableAlphabet, displayEntry, initSearch, initializeList, jargon, unhighlightLetters;
   $searchInput = document.getElementById('search');
   $output = document.getElementById('results');
+  $entriesBox = document.getElementById('entries');
+  $$letters = Array.prototype.slice.apply(document.querySelectorAll('.alphabet ul li a'));
+  $$entries = [];
   jargon = {};
+  unhighlightLetters = function() {
+    return $$letters.map(function(letterLi) {
+      return letterLi.classList.remove('highlighted');
+    });
+  };
+  createEntriesSelectableList = function(jargon) {
+    var anchor, key, li, results, value;
+    results = [];
+    for (key in jargon) {
+      value = jargon[key];
+      anchor = document.createElement("a");
+      anchor.href = "?term=" + key;
+      anchor.textContent = key;
+      li = document.createElement("li");
+      li.setAttribute("class", key.slice(0, 1).toLowerCase());
+      li.appendChild(anchor);
+      $entriesBox.appendChild(li);
+      $$entries = $entriesBox.querySelectorAll('li');
+      results.push($$entries = Array.prototype.slice.apply($$entries));
+    }
+    return results;
+  };
+  createSelectableAlphabet = function() {
+    return document.getElementById("alphabet").addEventListener("click", function(e) {
+      var letter;
+      e.preventDefault();
+      e.stopPropagation();
+      unhighlightLetters();
+      e.target.classList.add('highlighted');
+      letter = e.target.textContent.toLowerCase();
+      $output.querySelector('.entry').innerHTML = "";
+      $entriesBox.classList.remove("hide");
+      $entriesBox.classList.add("fold");
+      return $$entries.map(function(li) {
+        if (li.classList.contains(letter)) {
+          return li.classList.add('selected');
+        } else {
+          return li.classList.remove('selected');
+        }
+      });
+    });
+  };
+  displayEntry = function(key) {
+    $entriesBox.classList.add("hide");
+    $output.querySelector('.entry').innerHTML = jargon[key];
+    return location.href = location.href.split("#")[0] + "#" + key;
+  };
+  initializeList = function() {
+    return $entriesBox.addEventListener("click", function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      return displayEntry(e.target.textContent);
+    });
+  };
   initSearch = function(data) {
     var entries, h;
     entries = data.map(function(entry) {
       jargon[entry.name] = entry.html;
       return entry.name;
     });
-    h = horsey($searchInput, {
+    if (location.hash !== "") {
+      displayEntry(location.hash.slice(1));
+    }
+    createEntriesSelectableList(jargon);
+    createSelectableAlphabet();
+    initializeList();
+    return h = horsey($searchInput, {
       suggestions: entries,
       limit: 8,
-      render: function(li, suggestion) {
-        return li.innerHTML = suggestion;
-      },
       set: function(value) {
         $searchInput.value = value;
-        return $output.innerHTML = jargon[value];
+        displayEntry(value);
+        return unhighlightLetters();
       }
     });
-    return $searchInput.addEventListener('horsey-filter', function(e) {});
   };
   request.open('GET', 'js/entries.json', true);
   request.onload = function() {
